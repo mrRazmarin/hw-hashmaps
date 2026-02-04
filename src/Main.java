d70c87f927937e14f1df6e6ca55085c6f0b44037
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-02-04 12:02:05 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import code.Address;
+
+import java.util.*;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Address kazan = new Address("Россия", "Казань");
+        Address moscow = new Address("Россия", "Москва");
+        Address rio = new Address("Аргентина", "Рио-Де-Жанейро");
+
+        Map<Address, Integer> costPerAddress = new HashMap<>();
+
+        costPerAddress.put(kazan, 500);
+        costPerAddress.put(moscow, 1000);
+        costPerAddress.put(rio, 4500);
+
+        int totalSum = 0;
+
+        while(true) {
+
+            System.out.println("Заполнение нового заказа.");
+            System.out.print("Введите страну: ");
+            String inputCountry = scanner.nextLine();
+
+            if (inputCountry.equalsIgnoreCase("end")) {
+                break;
+            }
+
+            System.out.print("Введите город: ");
+            String inputCity = scanner.nextLine();
+
+            System.out.print("Введите вес (кг): ");
+            int inputWeight = Integer.parseInt(scanner.nextLine());
+
+            Address currentAddress = new Address(inputCountry, inputCity);
+
+            if (costPerAddress.containsKey(currentAddress)){
+                int costPerKg = costPerAddress.get(currentAddress);
+                int deliveryCost = costPerKg * inputWeight;
+
+                totalSum += deliveryCost;
+
+                System.out.printf("Стоимость доставки: %d%n", deliveryCost);
+                System.out.printf("Общая сумма всех доставок: %d%n", totalSum);
+                System.out.println();
+            } else {
+                System.out.println("Извините, такой адрес не обслуживается.");
+                System.out.println();
+            }
+        }
+    }
 }
--- src/code/Address.java
+++ src/code/Address.java
@@ -1,4 +1,41 @@
 package code;
 
+import java.util.Objects;
+
 public class Address {
+    private String country;
+    private String city;
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(country, address.country) && Objects.equals(city, address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city);
+    }
 }
